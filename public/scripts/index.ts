ca80421bc728e172eb2f9000f5596b608eae81cc
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2020-03-26 20:06:25 -0700

    Invoked the human control interface

--- public/scripts/index.ts
+++ public/scripts/index.ts
@@ -3,5 +3,6 @@ import MineGrid from "./MineGrid";
 
 const gridCanvas = new Canvas("canvas", "sans-serif");
 const mineGrid = new MineGrid(25, 25, 10, 100);
+mineGrid.giveToHuman(gridCanvas);
 
 while (true) mineGrid.draw(gridCanvas);
