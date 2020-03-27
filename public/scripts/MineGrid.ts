20f0aa74bf3509c07f390ea503a22ef67472356a
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2020-03-26 19:44:28 -0700

    First version of the MineGrid class

---
+++ public/scripts/MineGrid.ts
@@ -0,0 +1,87 @@
+import Canvas2D from "./Canvas2D";
+
+export default class MineGrid {
+    tileSize: number;
+    numBombs: number;
+    #grid: Array<Array<number>>;
+    #revealed: Array<Array<boolean>>;
+
+    constructor(
+        rows: number,
+        cols: number,
+        tileSize: number,
+        numBombs: number
+    ) {
+        this.tileSize = tileSize;
+        this.numBombs = numBombs;
+
+        const totalTiles = rows * cols;
+        let remainingBombs = numBombs;
+        this.#grid = new Array(rows);
+        this.#revealed = new Array(rows);
+        for (let i = 0; i < this.#grid.length; i++) {
+            this.#grid[i] = new Array(cols);
+            this.#revealed[i] = new Array(cols);
+            for (let j = 0; j < this.#grid[i].length; j++) {
+                if (
+                    remainingBombs > 0 &&
+                    Math.random() < remainingBombs / totalTiles
+                )
+                    this.#grid[i][j] = -1;
+                // A bomb
+                else this.#grid[i][j] = 0; // A blank
+
+                this.#revealed[i][j] = false;
+            }
+        }
+
+        const countNeighborBombs = (x: number, y: number) => {
+            let count = 0;
+            for (let i = -1; i <= 1; i++)
+                for (let j = 0; j <= 1; j++) {
+                    const xn = x + i;
+                    const yn = y + j;
+                    if (xn == x && yn == y) continue;
+                    if (xn >= 0 && xn < rows && yn >= 0 && yn < cols) {
+                        if (this.#grid[xn][yn] == -1) count++;
+                    }
+                }
+
+            return count;
+        };
+        for (let i = 0; i < this.#grid.length; i++) {
+            for (let j = 0; j < this.#grid[i].length; j++) {
+                this.#grid[i][j] = countNeighborBombs(i, j);
+            }
+        }
+    }
+
+    validTilePos(x: number, y: number) {
+        return (
+            x < this.#grid.length &&
+            x >= 0 &&
+            y < this.#grid[x].length &&
+            y >= 0
+        );
+    }
+
+    tileAt(x: number, y: number) {
+        // All positions outside the grid are unrevealed
+        if (!this.validTilePos(x, y)) return -1;
+
+        // consult the revealing matrix
+        if (!this.#revealed[x][y]) return -1;
+        else return this.#grid[x][y];
+    }
+
+    clickTile(x: number, y: number) {
+        if (!this.validTilePos(x, y)) return true;
+
+        this.#revealed[x][y] = true;
+        return this.tileAt(x, y) != -1;
+    }
+
+    draw(canvas: Canvas2D) {
+        console.log("Drawing");
+    }
+}
