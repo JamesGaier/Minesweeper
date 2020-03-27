5edfaf40b2f0bf82c6626fcae0044db855a5a396
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2020-03-26 19:45:27 -0700

    Added a basic HTML to JS hook

---
+++ public/scripts/index.ts
@@ -0,0 +1,7 @@
+import Canvas from "./Canvas2D";
+import MineGrid from "./MineGrid";
+
+const gridCanvas = new Canvas("canvas", "sans-serif");
+const mineGrid = new MineGrid(25, 25, 10, 100);
+
+while (true) mineGrid.draw(gridCanvas);
